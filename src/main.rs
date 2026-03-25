32e761c0e74212c826358b92db4f1c1074bcd302
Alexandre Denoyel <denoyela@gmail.com>
2026-03-25 20:09:59 +0100

    remove comments + corrections suggested during submit on exercism

--- src/main.rs
+++ src/main.rs
@@ -14,57 +14,50 @@ pub fn annotate(garden: &[&str]) -> Vec<String> {
         prev = cur.clone();
         cur = String::from(garden[i]);
         if i + 1 < garden.len() { next = String::from(garden[i+1]); } else { next = String::new(); };
-        // cur = String::from("7");
-        // let mut cur_pos = 0;
 
         let mut nb_neighbor;
 
+        // Handle flower logic here
         for cur_pos in 0 .. cur.len() {
             nb_neighbor = 0;
             if cur.chars().nth(cur_pos) == Some('*') {
-                // Handle flower logic here
+                
                 // No change on cur_pos when a flower is found
-                //copyvec.push(String::from(cur.clone()));
                 println! ("star found at row {}, cur_pos = {}", i, cur_pos);
 
             } else if cur.chars().nth(cur_pos) == Some(' ') {
                 if cur_pos > 0 {
-                    if cur.chars().nth(cur_pos-1) == Some('*') {nb_neighbor = nb_neighbor+1;}
-                    if ! prev.is_empty() && prev.chars().nth(cur_pos-1) == Some('*') {nb_neighbor = nb_neighbor+1;}
-                    if ! next.is_empty() && next.chars().nth(cur_pos-1) == Some('*') {nb_neighbor = nb_neighbor+1;}
+                    if cur.chars().nth(cur_pos-1) == Some('*') {nb_neighbor += 1;}
+                    if ! prev.is_empty() && prev.chars().nth(cur_pos-1) == Some('*') {nb_neighbor += 1;}
+                    if ! next.is_empty() && next.chars().nth(cur_pos-1) == Some('*') {nb_neighbor += 1;}
                 }
                 if cur_pos < cur.len()-1 {  
-                    if cur.chars().nth(cur_pos+1) == Some('*') {nb_neighbor = nb_neighbor+1;}
-                    if ! prev.is_empty() && prev.chars().nth(cur_pos+1) == Some('*') {nb_neighbor = nb_neighbor+1;}
-                    if ! next.is_empty() && next.chars().nth(cur_pos+1) == Some('*') {nb_neighbor = nb_neighbor+1;}
+                    if cur.chars().nth(cur_pos+1) == Some('*') {nb_neighbor += 1;}
+                    if ! prev.is_empty() && prev.chars().nth(cur_pos+1) == Some('*') {nb_neighbor += 1;}
+                    if ! next.is_empty() && next.chars().nth(cur_pos+1) == Some('*') {nb_neighbor += 1;}
                 }
-                if ! prev.is_empty() && prev.chars().nth(cur_pos) == Some('*') {nb_neighbor = nb_neighbor+1;}
-                if ! next.is_empty() && next.chars().nth(cur_pos) == Some('*') {nb_neighbor = nb_neighbor+1;}
+                if ! prev.is_empty() && prev.chars().nth(cur_pos) == Some('*') {nb_neighbor += 1;}
+                if ! next.is_empty() && next.chars().nth(cur_pos) == Some('*') {nb_neighbor += 1;}
                 if nb_neighbor == 0 {
-                    // copyvec.push(String::from(cur.clone()));
+                    
                     println! ("no neighbor found at row {}, cur_pos = {}", i, cur_pos);
                 }
                 else {
                     println! ("row = {} | cur_pos = {} | nb_neighbor = {}", i, cur_pos, nb_neighbor);
-                    // cur.chars().nth(cur_pos) = String::from("A");
-                    // cur = String::from("A");
+                    
                     let begin : String = cur[0..cur_pos].into();
                     let end : String = cur[cur_pos+1..].into();
                     cur = format!("{}{}{}", begin, nb_neighbor, end);
                 }
             }
         } // for loop on cur_pos
-        copyvec.push(String::from(cur.clone()));
+
+        copyvec.push(cur.clone());
         println! ("prev = {}|cur = {}|next = {}|", prev, cur, next);
-        // copyvec.push(String::from(garden[i]));
+        
     }; // for loop on garden[i]
-    // let mut a : Vec::<String> = Vec::new();
-    // a.push(String::from(" 2*2 "));
-    // a.push(String::from("25*52"));
-    // a.push(String::from("*****"));
-    // a.push(String::from("25*52"));
-    // a.push(String::from(" 2*2 "));
-    return copyvec;
+    
+    copyvec
 } // fn annotate
 
 fn main() {
